ef363db4b0c5e4ec055071e1c37fc215966eb5d4
Helen Zeng <helen.w.zeng@gmail.com>
2013-11-10 13:28:22 -0800

    checkin' dem tickets

--- scraper.js
+++ scraper.js
@@ -3,30 +3,38 @@ var cheerio = require('cheerio');
 
 var url = "https://helen-sccc.eventbrite.com/";
 
-request(url, function (err, res, body) {
-  if (err)
-    throw err;
-  $ = cheerio.load(body, { ignoreWhitespace: true });
-  $('.ticket_table .ticket_row').each(function () {
-    
-    //gets 3rd child (status) of the ticket body
-    var status = (this.find('td:nth-child(3)').text().replace(/\s+/g, ""));
-    
-    if (status != 'NotStarted') {
-      this.find(".ticket_type_name").each(function () {
-      var ticketType = $(this).text().replace(/\s+/g, "");
-      console.log(ticketType);
-      });
-      
-      this.find("#remaining_quant_21257607").each(function () {
-      var numTicket = $(this).text().replace(/\s+/g, "");
-      var regex = ".*?(?=Ticket)";
-      var match = numTicket.match(regex)[0];
-      console.log(match);
-      });
-    
-    } else {
-      console.log("Not Started");
-    }
+var checkInterval = setInterval(checkEvent, 10000); //10 seconds
+
+function checkEvent() {
+  request(url, function (err, res, body) {
+    if (err)
+      throw err;
+    $ = cheerio.load(body, { ignoreWhitespace: true });
+    $('.ticket_table .ticket_row').each(function () {
+
+      //gets 3rd child (status) of the ticket body
+      var status = (this.find('td:nth-child(3)').text().replace(/\s+/g, ""));
+
+      if (status != 'NotStarted') {
+        
+        //stop checking
+        clearInterval(checkInterval);
+
+        this.find(".ticket_type_name").each(function () {
+          var ticketType = $(this).text().replace(/\s+/g, "");
+          console.log(ticketType);
+        });
+
+        this.find("#remaining_quant_21257607").each(function () {
+          var numTicket = $(this).text().replace(/\s+/g, "");
+          var regex = ".*?(?=Ticket)";
+          var match = numTicket.match(regex)[0];
+          console.log(match);
+        });
+
+      } else {
+        console.log("Not Started");
+      }
+    });
   });
-});
+};
